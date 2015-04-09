6dc725bf69df4de9a032cfc794c4ecc3053b759f
Jose A. Araujo <entropiccow@gmail.com>
2015-04-09 15:04:33 -0500

    Mono BigInteger Format workaround.
    Same as the Winforms version.

--- Gem.cs
+++ Gem.cs
@@ -15,18 +15,18 @@ namespace D3GemCalculatorWPF
         int[] GoldChart;
         int[] DeathsBreathChart;
 
-        public Gem() 
+        public Gem()
         { // this used to be 3 txt files read from assembly and stored in a multi-dimensional array, but this is more efficient
-            GemChart = new int[18] {2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
-            GoldChart = new int[18]{10, 25, 40, 55, 70, 85, 100, 5000, 10000, 20000, 30000, 50000, 75000, 100000, 200000, 300000, 400000, 500000};
-            DeathsBreathChart = new int[18]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1};
+            GemChart = new int[18] { 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 };
+            GoldChart = new int[18] { 10, 25, 40, 55, 70, 85, 100, 5000, 10000, 20000, 30000, 50000, 75000, 100000, 200000, 300000, 400000, 500000 };
+            DeathsBreathChart = new int[18] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 };
         }
 
         private string CalculateGold(int typeused, int typewanted, int amountwanted)
         {
             BigInteger goldresult = 0;
-            
-            
+
+
             for (int i = typeused; i < typewanted; i++)
             {
                 goldresult = goldresult * GemChart[i] + GoldChart[i];
@@ -34,32 +34,32 @@ namespace D3GemCalculatorWPF
 
 
             goldresult = BigInteger.Multiply(goldresult, amountwanted);
-            return goldresult.ToString("#,#") + " Gold needed.";
+            return convertBigNumber(goldresult) + " Gold needed.";
         }
         private string CalculateGemsNeeded(int typeused, int typewanted, int amountwanted, string gemtype)
         {
             BigInteger gemresult = 0;
 
-            for (int i = typeused; i < typewanted; i++ )
+            for (int i = typeused; i < typewanted; i++)
             {
-                gemresult = (gemresult > 0) ?  gemresult * GemChart[i] : gemresult + GemChart[i];
+                gemresult = (gemresult > 0) ? gemresult * GemChart[i] : gemresult + GemChart[i];
             }
 
-               gemresult = BigInteger.Multiply(gemresult, amountwanted);
-            return gemresult.ToString("#,#") + " " + gemtype + " needed.";
+            gemresult = BigInteger.Multiply(gemresult, amountwanted);
+            return convertBigNumber(gemresult) + " " + gemtype + " needed.";
         }
         private string CalculateDeathsBreath(int typeused, int typewanted, int amountwanted)
         {
             BigInteger deathsbreathresult = 0;
 
-            for (int i = typeused; i < typewanted; i++ )
-            {         
-              deathsbreathresult = (deathsbreathresult > 0) ? deathsbreathresult * DeathsBreathChart[i] + GemChart[i] : DeathsBreathChart[i];
+            for (int i = typeused; i < typewanted; i++)
+            {
+                deathsbreathresult = (deathsbreathresult > 0) ? deathsbreathresult * DeathsBreathChart[i] + GemChart[i] : DeathsBreathChart[i];
             } // I am not sure why I did it like this, but it seems to work, so I left it alone.
 
             deathsbreathresult = BigInteger.Multiply(deathsbreathresult, amountwanted);
             if (deathsbreathresult > 0)
-                return deathsbreathresult.ToString("#,#") + " Death's Breath needed."; 
+                return convertBigNumber(deathsbreathresult) + " Death's Breath needed.";
             else
                 return ""; //no need to render text for death's breath output if none are needed
 
@@ -73,24 +73,24 @@ namespace D3GemCalculatorWPF
             long timeresult = 0;
             string Seconds, Minutes, Hours;
 
-            for (int i = typeused; i < typewanted; i++ )
+            for (int i = typeused; i < typewanted; i++)
             {
-                timeresult = (timeresult >0) ? timepercombine + timeresult * GemChart[i] : timepercombine;
+                timeresult = (timeresult > 0) ? timepercombine + timeresult * GemChart[i] : timepercombine;
             } //it works, that's all I know.
             timeresult = timeresult * amountwanted;
             minutes = Math.DivRem(timeresult, 60, out seconds); //some modulus/remainder division to get minutes and seconds
             hours = Math.DivRem(minutes, 60, out minutes); // same for hours/minutes
             Seconds = seconds.ToString(); //turn those longints into strings for final processing
             Minutes = minutes.ToString();
-            Hours = hours.ToString("#,#");
+            Hours = hours.ToString("N0");
 
-            return("Total Time: " + Hours.PadLeft(2, '0') + ":" + Minutes.PadLeft(2, '0') + ":" + Seconds.PadLeft(2, '0')); //padded output
+            return ("Total Time: " + Hours.PadLeft(2, '0') + ":" + Minutes.PadLeft(2, '0') + ":" + Seconds.PadLeft(2, '0')); //padded output
         }
 
         public string[] SanityCheck(int typeused, int typewanted, string amountwanted, object gemtypeused, object gemtypewanted)
         { // new and improved sanity check code. replaces the old error checking in buttonclick event method thing.
-            int quantity = 0;
             string[] output;
+            int quantity;
 
             if (typeused == -1 && typewanted == -1)
             {
@@ -122,19 +122,14 @@ namespace D3GemCalculatorWPF
                 output[0] = "Gem used is greather than Gem wanted.";
                 return output;
             }
-            else if (amountwanted == "" || amountwanted == "0")
+            else if (!amountwanted.All(char.IsDigit)) //used to do fancy checks for letters and such, now just checks if the entire string is numbers.
             {
                 output = new string[1];
-                output[0] = "Please enter a number.";
-                return output;
-            }
-            else if (!int.TryParse(amountwanted, out quantity))
-            {
-                output = new string[1];
-                output[0] = amountwanted + " is not a number.";
+                output[0] = "Whole numbers only.";
                 return output;
             }
             else
+                quantity = int.Parse(amountwanted);
                 output = new string[6];
             output[0] = CalculateGemsNeeded(typeused, typewanted, quantity, gemtypeused.ToString());
             output[1] = CalculateGold(typeused, typewanted, quantity);
@@ -142,6 +137,33 @@ namespace D3GemCalculatorWPF
             output[3] = CalculateTime(typeused, typewanted, quantity);
             output[4] = "To make " + quantity + " " + gemtypewanted.ToString() + ".";
             output[5] = "Using " + gemtypeused.ToString() + ".";
+            return output;
+        }
+
+        private string convertBigNumber(BigInteger input) //custom method for overcoming Mono biginteger's lack of Format method. only commas for now.
+        {
+            string output = "";
+            string temp;
+            char[] tempArray;
+            char[] tempArrayTwo;
+
+            temp = input.ToString(); // take big int and turn it into string
+            tempArray = temp.ToCharArray(); // to char array
+            Array.Reverse(tempArray); // reverse  it
+            for (int i = 0; i != tempArray.Length; ++i)
+            {
+                if (i % 3 != 0 || i == 0)
+                {
+                    output = output + tempArray[i];
+                }
+                if (i != 0 && i % 3 == 0 && i != tempArray.Length)
+                {
+                    output = output + "," + tempArray[i];
+                }
+            }
+            tempArrayTwo = output.ToCharArray();
+            Array.Reverse(tempArrayTwo); // one final array reverse then reconversion
+            output = new string(tempArrayTwo);
             return output;
         }
     }
